41783fa2f77887e3a2af371ed8a65899a383a466
jrh154 <john.hayes@usask.ca>
2019-05-08 21:23:18 -1000

    things

--- devices.py
+++ devices.py
@@ -2,8 +2,8 @@ import lib.device_library as dl
 from lib.maps import piMap
 
 class deviceList():
-	map = piMap()
+	m = piMap()
 	
 	## Initialize your devices (motors, sensors, etc) here. For example, start a motor here
-	testServo = dl.rovServo(testServo_pin)
-	testDualEsc = dl.rovBrushlessDualESC(testMotorDualESC_fpin, testMotorDualESC_rpin)
+	testServo = dl.rovServo(m.testServo_pin)
+	testDualEsc = dl.rovBrushlessDualESC(m.testMotorDualESC_fpin, m.testMotorDualESC_rpin)
